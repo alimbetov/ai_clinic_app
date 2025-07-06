22ad414da1a963294b61031f446390f9c9433296
alimbetov <alimbetovpost@gmail.com>
2025-07-06 12:17:39 +0500

    waveF2

--- lib/core/utils/image_picker_helper.dart
+++ lib/core/utils/image_picker_helper.dart
@@ -1,16 +1,16 @@
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:universal_io/io.dart';
 
 class ImagePickerHelper {
   static Future<File?> pickImage() async {
     if (Platform.isAndroid || Platform.isIOS) {
       final picker = ImagePicker();
-      final picked = await picker.pickImage(source: ImageSource.gallery);
-      if (picked != null) return File(picked.path);
-    } else {
-      // Desktop
+      final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+      if (pickedFile != null) {
+        return File(pickedFile.path);
+      }
+    } else if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
       final result = await FilePicker.platform.pickFiles(
         type: FileType.image,
         allowMultiple: false,
